20edc0eae17c63a9a3bfdb003eda4b8c4cf63f5d
Tomasz Tarnogrodzki <tomasz.tarnogrodzki@gmail.com>
2018-06-06 22:38:47 +0200

    stringutils, testing

--- hello/hello.go
+++ hello/hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
-
+import (
+        "fmt"
+        "github.com/ttarnogrodzki/learngo/stringutil"
+      )
 func main() {
   fmt.Printf("hello, world\n")
+  fmt.Printf( stringutil.Reverse( "!oG ,olleH"))
 }
